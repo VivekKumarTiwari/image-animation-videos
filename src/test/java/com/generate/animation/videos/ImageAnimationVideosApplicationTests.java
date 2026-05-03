89e01578cc2e2fc317bd4159f64ba9932b421a54
vivek <vivektiwari.tiwari125@gmail.com>
2026-05-03 08:07:51 +0530

    create project

---
+++ src/test/java/com/generate/animation/videos/ImageAnimationVideosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generate.animation.videos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageAnimationVideosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
